86f604c0ecfb03f0f998233e072afb3505a63d98
hi-lufz1 <latifufauzi@gmail.com>
2023-05-22 19:50:36 +0700

    15

---
+++ mainJM/manusia.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "Jantung dihidupkan \n";
+	}
+	
+};
+#endif
