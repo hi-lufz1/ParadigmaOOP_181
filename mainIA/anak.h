3fe89d827a7f01281a75c5491060826b35ed61b3
hi-lufz1 <latifufauzi@gmail.com>
2023-05-22 15:38:10 +0700

    5

--- mainIA/anak.h
+++ mainIA/anak.h
@@ -7,13 +7,11 @@ public:
 	string nama;
 	anak(string pNama) : nama(pNama) {
 		cout << "Anak \"" << nama << "\" ada\n";
-
-
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
 	}
 };
-
-
-
 
 #endif
 
--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -1,3 +1,4 @@
 #include <iostream>
+#include "anak.h"
 
 using namespace std;
