0a4d9b81328aa617cad7733fef809b365728fef5
hi-lufz1 <latifufauzi@gmail.com>
2023-05-22 19:58:27 +0700

    16

---
+++ mainJM/manusia.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+#include <iostream>;
+#include "jantung.h"
+using namespace std;
+
+class manusia {
+public:
+	string nama;
+	jantung varJantung;
+
+	
+};
+#endif
