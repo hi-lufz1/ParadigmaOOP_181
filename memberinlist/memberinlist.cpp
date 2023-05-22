94f960c7cd6703ed9609b9b2ed5ab4fd943809b1
hi-lufz1 <latifufauzi@gmail.com>
2023-05-22 15:23:29 +0700

    1

--- memberinlist/memberinlist.cpp
+++ memberinlist/memberinlist.cpp
@@ -2,3 +2,19 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private :
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+
+	}
+	~mahasiswa() {
+		cout << "id = " << id << endl;
+		cout << "nama = ";
+
+	}
+};
